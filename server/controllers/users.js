9fcf2a407153e0cdbe279ed08cafc134e8bd7514
halisa <halisasaipulla@gmail.com>
2023-03-28 06:23:42 -0700

    created get user friends route

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -12,3 +12,20 @@ export const getUser = async (req, res) => {
     }
 }
 
+export const getUserFriends = async (req, res) =>{
+    try{
+        const { id } = req.params;
+        const user = await User.findId(id);
+        const friends = await Promise.all(
+            user.friends.map((id)=> User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) =>{
+                return { _id, firstName, lastName, occupation, location, picturePath }
+            }
+        );
+        res.status(200).json(formattedFriends);
+    }catch(err){
+        res.status(404).json({message: err.message});
+    }
+}
