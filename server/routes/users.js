7f209d06f2b0c5c5e2d7ae2ac1a5af425928382d
halisa <halisasaipullaa@gmail.com>
2023-03-28 06:23:42 -0700

    created get user friends route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getUser } from '../controllers/users.js';
+import { getUser,getUserFriends } from '../controllers/users.js';
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 // Read
 router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
 
 export default router;
