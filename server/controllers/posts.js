be468960914e5cecb67b98182fcb76b8a81be49d
halisa <halisasaipulla@gmail.com>
2023-03-28 10:52:14 -0700

    created like pest by id  route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,5 +45,31 @@ export const getUserPosts = async (req, res) => {
     }
 };
 
+// Update 
+export const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+    
+        if (isLiked) {
+            post.likes.delete(userId);
+        } else {
+            post.likes.set(userId, true);
+        }
+    
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true }
+        );
+    
+        res.status(200).json(updatedPost);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
 
 
