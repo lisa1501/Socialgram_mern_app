fc00c3eb2f6f8583f974f871316e59d265993b72
halisa <halisasaipullaa@gmail.com>
2023-03-29 08:58:49 -0700

    created React Redux File Folder and React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+
 function App() {
+    
     return (
         <div className="app">
-            hello world!
+            <BrowserRouter>
+
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/home" element={<HomePage /> } />
+                    <Route path="/profile/:userId" element={ <ProfilePage /> } />
+                </Routes>
+        
+            </BrowserRouter>
         </div>
     );
 }
