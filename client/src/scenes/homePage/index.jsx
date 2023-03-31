55a72e354c48a0197153763657d6eca938ca0ac7
halisa <halisasaipulla@gmail.com>
2023-03-31 12:03:04 -0700

    successfully register a new user from client site

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import LoginPage from 'scenes/loginPage';
+
 import Navbar from 'scenes/navbar';
 
 const HomePage = () => {
     return (
         <div>
-            {/* <Navbar /> */}
-            <LoginPage />
+            <Navbar />
         </div>
     )
 }
