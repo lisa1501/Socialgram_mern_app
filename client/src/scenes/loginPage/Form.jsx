bd84ed5f6095496f980cbce9f9f519ae703ed30c
halisa <halisasaipullaa@gmail.com>
2023-03-31 10:15:30 -0700

    rendered the user login form

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -34,20 +34,29 @@ const initialValuesRegister = {
     occupation: "",
     picture: "",
 };
+const loginSchema = yup.object().shape({
+    email: yup.string().email("invalid email").required("required"),
+    password: yup.string().required("required"),
+});
+const initialValuesLogin = {
+    email: "",
+    password: "",
+};
 
 const Form = () => {
-    const [pageType, setPageType] = useState("register");
+    const [pageType, setPageType] = useState("login");
     const { palette } = useTheme();
     const isNonMobile = useMediaQuery("(min-width:600px)");
     const isRegister = pageType === "register";
+    const isLogin = pageType === "login";
 
     const handleFormSubmit = async () => {};
 
     return (
         <Formik
             onSubmit={handleFormSubmit}
-            initialValues={initialValuesRegister}
-            validationSchema={registerSchema}
+            initialValues={initialValuesLogin}
+            validationSchema={loginSchema}
         >
             {({
                 values,
@@ -149,7 +158,46 @@ const Form = () => {
                             </>
                         )}
                         
-                    </Box>        
+                        <TextField
+                            label="Email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.email}
+                            name="email"
+                            error={Boolean(touched.email) && Boolean(errors.email)}
+                            helperText={touched.email && errors.email}
+                            sx={{ gridColumn: "span 4" }}
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.password}
+                            name="password"
+                            error={Boolean(touched.password) && Boolean(errors.password)}
+                            helperText={touched.password && errors.password}
+                            sx={{ gridColumn: "span 4" }}
+                        />
+                    
+                    </Box>  
+
+                    {/* BUTTONS */}
+                    <Box>
+                        <Button
+                            fullWidth
+                            type="submit"
+                            sx={{
+                                m: "2rem 0",
+                                p: "1rem",
+                                backgroundColor: palette.primary.main,
+                                color: palette.background.alt,
+                                "&:hover": { color: palette.primary.main },
+                            }}
+                        >
+                            {"Login"}
+                        </Button>
+                    </Box>
                 </form>
             )}
         </Formik>
