6dd252d28a543ced060451c5f4024460cca57237
halisa <halisasaipullaa@gmail.com>
2023-04-01 13:13:17 -0700

    fixed the issue -- edited User model viewedProfile

--- server/models/User.js
+++ server/models/User.js
@@ -35,7 +35,7 @@ const UserSchema = new mongoose.Schema(
         },
         location: String,
         occupation: String,
-        viewProfile: Number,
+        viewedProfile: Number,
         impressions: Number,
     },
     {timestamps: true}
