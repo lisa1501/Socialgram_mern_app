cd642b805f2f7f94fe5d5a0eb6a774b157e0fe95
halisa <halisasaipulla@gmail.com>
2023-04-02 09:04:45 -0700

    rendered add image

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -1,24 +1,84 @@
 import { useState } from "react";
-import { InputBase } from "@mui/material";
+import {
+    IconButton,
+    Box,
+    Divider,
+    Typography,
+    InputBase,
+} from "@mui/material";
+import {
+    EditOutlined,
+    DeleteOutlined,
+    ImageOutlined,
+} from "@mui/icons-material";
 
+import FlexBetween from "components/FlexBetween";
+import Dropzone from "react-dropzone";
+import UserImage from "components/UserImage";
 import WidgetWrapper from "components/WidgetWrapper";
-import FlexBetween from 'components/FlexBetween';
-import UserImage from 'components/UserImage';
 
-const MyPostWidget = ({picturePath}) => {
+const MyPostWidget = ({ picturePath }) => {
     const [post, setPost] = useState("");
+    const [isImage, setIsImage] = useState(false);
+    const [image, setImage] = useState(null);
+    
     return (
         <WidgetWrapper>
             <FlexBetween gap="1.5rem">
                 <UserImage image={picturePath} />
-                <InputBase 
+                <InputBase
                     placeholder="What's on your mind..."
                     onChange={(e) => setPost(e.target.value)}
                     value={post}
                 />
             </FlexBetween>
-        </WidgetWrapper>
-    )
-}
+            {isImage && (
+                <Box>
+                    <Dropzone
+                        acceptedFiles=".jpg,.jpeg,.png"
+                        multiple={false}
+                        onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+                    >
+                    {({ getRootProps, getInputProps }) => (
+                        <FlexBetween>
+                            <Box
+                                {...getRootProps()}
+                            >
+                                <input {...getInputProps()} />
+                                {!image ? (
+                                <p>Add Image Here</p>
+                                ) : (
+                                <FlexBetween>
+                                    <Typography>{image.name}</Typography>
+                                    <EditOutlined />
+                                </FlexBetween>
+                                )}
+                            </Box>
+                            {image && (
+                                <IconButton
+                                    onClick={() => setImage(null)}
+                                >
+                                    <DeleteOutlined />
+                                </IconButton>
+                            )}
+                            </FlexBetween>
+                        )}
+                        </Dropzone>
+                    </Box>
+                )}
 
-export default MyPostWidget
+                <Divider sx={{ margin: "1.25rem 0" }} />
+
+                <FlexBetween>
+                    <FlexBetween onClick={() => setIsImage(!isImage)}>
+                        <ImageOutlined  />
+                        <Typography>
+                            Image
+                        </Typography>
+                    </FlexBetween>
+                </FlexBetween>
+        </WidgetWrapper>
+    );
+};
+
+export default MyPostWidget;
