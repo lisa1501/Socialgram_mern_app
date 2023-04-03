6f76e2dbdbfe846c7b356744fa206d4b8e9eedbe
Lisa S <halisasaipulla@gmail.com>
2023-04-03 16:05:45 -0700

    rendered all details about a single post

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -1,7 +1,14 @@
 import WidgetWrapper from 'components/WidgetWrapper'
-import { Typography } from "@mui/material";
+import { Typography, IconButton } from "@mui/material";
+import {
+    ChatBubbleOutlineOutlined,
+    FavoriteBorderOutlined,
+    ShareOutlined,
+} from "@mui/icons-material";
+import { useState } from "react";
 
 import UserInfo from "components/UserInfo";
+import FlexBetween from 'components/FlexBetween';
 
 const PostWidget = (
     {
@@ -16,7 +23,9 @@ const PostWidget = (
         comments,
     }
 ) => {
-    
+    const [isComments, setIsComments] = useState(false);
+    const likeCount = Object.keys(likes).length;
+
     return (
         <WidgetWrapper m="2rem 0">
             <UserInfo
@@ -26,7 +35,38 @@ const PostWidget = (
             />
             <Typography  sx={{ mt: "1rem" }}>
                 {description}
-            </Typography>    
+            </Typography>
+            {picturePath && (
+                <img
+                    width="100%"
+                    height="auto"
+                    alt="post"
+                    style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+                    src={`http://localhost:3001/assets/${picturePath}`}
+                />
+            )}
+            <FlexBetween mt="0.25rem">
+                <FlexBetween gap="1rem">
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={onclick}>
+                            <FavoriteBorderOutlined />
+                        </IconButton>
+                        <Typography>{likeCount}</Typography>
+                    </FlexBetween>
+
+                    <FlexBetween gap="0.3rem">
+                        <IconButton onClick={() => setIsComments(!isComments)}>
+                            <ChatBubbleOutlineOutlined />
+                        </IconButton>
+                        <Typography>{comments.length}</Typography>
+                    </FlexBetween>
+                </FlexBetween>
+
+                <IconButton>
+                    <ShareOutlined />
+                </IconButton>
+
+            </FlexBetween>
         </WidgetWrapper>
     )
 }
