20e9639a76d48fa6d51e906a902290c427dd449d
Lisa S <halisasaipulla@gmail.com>
2023-04-03 18:38:46 -0700

    implemented like button and comment button are clickable

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -1,11 +1,15 @@
 import WidgetWrapper from 'components/WidgetWrapper'
-import { Typography, IconButton } from "@mui/material";
+import { Typography, IconButton, Box, Divider,useTheme} from "@mui/material";
 import {
     ChatBubbleOutlineOutlined,
     FavoriteBorderOutlined,
     ShareOutlined,
+    FavoriteOutlined 
 } from "@mui/icons-material";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
+import { useNavigate } from "react-router-dom";
 
 import UserInfo from "components/UserInfo";
 import FlexBetween from 'components/FlexBetween';
@@ -24,15 +28,39 @@ const PostWidget = (
     }
 ) => {
     const [isComments, setIsComments] = useState(false);
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+    const loggedInUserId = useSelector((state) => state.user._id);
+    const isLiked = Boolean(likes[loggedInUserId]);
     const likeCount = Object.keys(likes).length;
+    const navigate = useNavigate();
+
+    const { palette } = useTheme();
+    const main = palette.neutral.main;
+    const primary = palette.primary.main;
+
+    const patchLike = async () => {
+        const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+            method: "PATCH",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userId: loggedInUserId }),
+            });
+            const updatedPost = await response.json();
+            dispatch(setPost({ post: updatedPost }));
+    };
 
     return (
-        <WidgetWrapper m="2rem 0">
+        <WidgetWrapper m="2rem 0"
+        >            
             <UserInfo
                 name={name}
                 subtitle={location}
-                userPicturePath={userPicturePath}
+                userPicturePath={userPicturePath }
             />
+            
             <Typography  sx={{ mt: "1rem" }}>
                 {description}
             </Typography>
@@ -43,13 +71,18 @@ const PostWidget = (
                     alt="post"
                     style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
                     src={`http://localhost:3001/assets/${picturePath}`}
+                    
                 />
             )}
             <FlexBetween mt="0.25rem">
                 <FlexBetween gap="1rem">
                     <FlexBetween gap="0.3rem">
-                        <IconButton onClick={onclick}>
+                        <IconButton onClick={patchLike}>
+                        {isLiked ? (
+                            <FavoriteOutlined sx={{ color: primary }} />
+                        ) : (
                             <FavoriteBorderOutlined />
+                        )}
                         </IconButton>
                         <Typography>{likeCount}</Typography>
                     </FlexBetween>
@@ -67,6 +100,20 @@ const PostWidget = (
                 </IconButton>
 
             </FlexBetween>
+
+            {isComments && (
+                <Box mt="0.5rem">
+                    {comments.map((comment, i) => (
+                        <Box key={`${name}-${i}`}>
+                            <Divider />
+                            <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                                {comment}
+                            </Typography>
+                        </Box>
+                    ))}
+                    <Divider />
+                </Box>
+            )}
         </WidgetWrapper>
     )
 }
